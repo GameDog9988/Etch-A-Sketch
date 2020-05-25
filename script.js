a8c17af23623a5091e439d2a2e5f0ed9f734c16d
GameDog9988 <51346343+GameDog9988@users.noreply.github.com>
2020-05-25 09:33:54 -0400

    Create 16x16 grid

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let container = document.getElementById("container");
-let sketchGrid = "";
+const container = document.getElementById("container");
 
-for (i = 0; i < 17; i++) {
-  sketchGrid += '<div class="box"></div>';
+function makeRows(rows, cols) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    cell.innerText = c + 1;
+    container.appendChild(cell).className = "grid-item";
+  }
 }
-container.innerHTML = sketchGrid;
+
+makeRows(16, 16);
